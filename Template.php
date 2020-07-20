279107ae43ee864e8995ba33650c2dd2930e5c0d
ManameTaira <65719610+ManameTaira@users.noreply.github.com>
2020-07-20 18:21:17 -0300

    Criar template

--- Template.php
+++ Template.php
@@ -4,31 +4,28 @@
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <title><?php echo $title; ?></title>
         <link rel="stylesheet" type="text/css" href="assets/style.css" />
+        <link href="https://fonts.googleapis.com/css2?family=Pinyon+Script&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Volkhov:ital,wght@1,700&display=swap" rel="stylesheet">
     </head>
     <body>
-        <div id="wrapper">
-            <div id="banner">
-            </div>
-
-            <nav id="navigation">
-                <ul id="nav">
-                    <li><a href="index.php">Home</a></li>
-                    <li><a href="#">Coffee</a></li>
-                    <li><a href="#">Shop</a></li>
-                    <li><a href="#">About</a></li>
+        <div class="old-paper">
+            <nav class="menu">
+                <ul>
+                    <li><a href="index.php">Treasure Hunt</a></li>
+                    <li><a href="#">Instruções</a></li>
+                    <li><a href="#">Mapa</a></li>
+                    <li><a href="#">Sobre</a></li>
+                    <li><a href="#">Perguntas</a></li>
                 </ul>
             </nav>
-
             <div id="content_area">
                 <?php echo $content; ?>
             </div>
 
-            <div id="sidebar">
-
-            </div>
-
             <footer>
+              <hr>
                 <p>All rights reserved</p>
+              <hr>
             </footer>
         </div>
     </body>
--- index.php
+++ index.php
@@ -1,17 +1,22 @@
 <?php
 $title = "Home";
 $content = '
-<h3>Title 1</h3>
-<p>
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum lectus urna,
-    viverra in luctus quis, ullamcorper quis lorem. Vestibulum vulputate pellentesque
-    velit, et placerat dolor pulvinar in. Class aptent taciti sociosqu ad litora torquent
-    per conubia nostra, per inceptos himenaeos. Sed sit amet velit at purus elementum
-    dapibus. Nulla dapibus auctor vulputate. Sed cursus nisi at mauris mollis semper.
-    Vestibulum consectetur cursus dui sit amet pretium.
-</p>
+<hr>
+<h1>Home</h1>
+<hr>
 
-<h3>Title 2</h3>
+<h2>Title 1</h2>
+
+  <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum lectus urna,
+      viverra in luctus quis, ullamcorper quis lorem. Vestibulum vulputate pellentesque
+      velit, et placerat dolor pulvinar in. Class aptent taciti sociosqu ad litora torquent
+      per conubia nostra, per inceptos himenaeos. Sed sit amet velit at purus elementum
+      dapibus. Nulla dapibus auctor vulputate. Sed cursus nisi at mauris mollis semper.
+      Vestibulum consectetur cursus dui sit amet pretium.
+  </p>
+
+<h2>Title 2</h2>
 <p>
     Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos
     himenaeos. Donec porttitor placerat nunc a consectetur. Ut fermentum eleifend molestie.
@@ -25,7 +30,7 @@ $content = '
     tincidunt est ac elit bibendum nec varius ante vestibulum.
 </p>
 
-<h3>Title 3</h3>
+<h2>Title 3</h2>
 <p>
     In hac habitasse platea dictumst. In purus leo, consequat nec porta a, varius sed
     dui. Nulla placerat mollis tincidunt. Praesent at elit mi, a posuere quam. Suspendisse
@@ -33,7 +38,19 @@ $content = '
     Morbi arcu sem, rhoncus sit amet rutrum eget, interdum sit amet leo. Ut mattis lorem
     vitae dolor tincidunt mattis a lacinia velit. Cras sed nibh at urna imperdiet laoreet
     quis a risus.
+</p>
+<h2>Title 4</h2>
+<p>
+    Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos
+    himenaeos. Donec porttitor placerat nunc a consectetur. Ut fermentum eleifend molestie.
+    Donec fermentum risus sit amet ante cursus cursus. In hac habitasse platea dictumst.
+    Praesent semper ante ut felis molestie aliquet. Nam quis dui a magna molestie blandit
+    id et justo. Class aptent taciti sociosqu ad litora torquent per conubia nostra,
+    per inceptos himenaeos. Sed felis mauris, mattis a vulputate a, gravida nec metus.
+    Vivamus elit augue, ullamcorper eget scelerisque vitae, tincidunt eget lorem. Curabitur
+    id nibh libero, sed pulvinar nisi. Curabitur ultrices, neque in dignissim viverra,
+    justo nisl dignissim magna, a tempor eros turpis in diam. Suspendisse potenti. Sed
+    tincidunt est ac elit bibendum nec varius ante vestibulum.
 </p>';
-$canvas = "<canvas> </canvas>";
 include 'Template.php';
 ?>
